2eda24b01949799d99201c2f80199011272abd63
Kreliannn <krelianquimson@gmail.com>
2025-01-31 19:52:10 +0800

    add functionality for sign up

--- src/app/interface/account.ts
+++ src/app/interface/account.ts
@@ -4,3 +4,10 @@ export interface signInInterface {
     username : string,
     password : string
 }
+
+export interface signUpInterface  {
+    fullname : string,
+    username : string,
+    password : string,
+    confirmPassword : string
+}
--- src/app/pages/sign_up/page.tsx
+++ src/app/pages/sign_up/page.tsx
@@ -1,20 +1,39 @@
-
+"use client"
 import { TextField, Button } from "@mui/material"
+import { useState } from "react"
+import axios from "@/app/hooks/api"
+import { useMutation } from "@tanstack/react-query"
+import { signUpInterface } from "@/app/interface/account"
+import { AxiosError } from "axios"
 
 
 export default function SignUp()
 {
+    const [form, setForm] = useState({
+        fullname : "",
+        username : "",
+        password : "",
+        confirmPassword : ""
+    })
 
+    const formSetter = (field: string, value: string) => setForm((prev) => ({...prev, [field] : value}))
 
+    const mutation = useMutation({
+        mutationFn : (data: signUpInterface) => axios.post("/sign_up", data),
+        onSuccess : (response) => alert(response.data),
+        onError : (err) => alert((err as AxiosError<{ msg : string}>).response?.data.msg ),
+    })
+
+ 
     return(
         <div className="flex justify-center place-items-center w-full h-dvh">
             <div className="w-80 h-auto  border drop-shadow-lg bg-gray-100 p-5">
                 <h1 className="text-center text-2xl font-bold mb-3"> sign up </h1>
-                <TextField type="text"  label="fullname" variant="outlined" fullWidth className="mb-5" />
-                <TextField type="text"  label="username" variant="outlined" fullWidth className="mb-5" />
-                <TextField type="text"  label="password" variant="outlined" fullWidth className="mb-5" />
-                <TextField type="text"  label="confirm password" variant="outlined" fullWidth className="mb-5" />
-                <Button variant="contained" fullWidth> sign in </Button>
+                <TextField type="text"  label="fullname" variant="outlined" fullWidth className="mb-5" value={form.fullname} onChange={(e) => formSetter("fullname", e.target.value)}/>
+                <TextField type="text"  label="username" variant="outlined" fullWidth className="mb-5" value={form.username} onChange={(e) => formSetter("username", e.target.value)}/>
+                <TextField type="password"  label="password" variant="outlined" fullWidth className="mb-5" value={form.password} onChange={(e) => formSetter("password", e.target.value)}/>
+                <TextField type="password"  label="confirm password" variant="outlined" fullWidth className="mb-5" value={form.confirmPassword} onChange={(e) => formSetter("confirmPassword", e.target.value)}/>
+                <Button variant="contained" fullWidth onClick={() => mutation.mutate(form)}> sign in </Button>
             </div>
         </div>
     )
